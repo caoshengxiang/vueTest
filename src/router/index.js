3f9da2133cfd54b061b22b669b48cc776bea86dc
caoshengxiang <1104729815@qq.com>
2017-03-10 14:06:31 +0800

    router按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,32 +3,48 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-import Hello from 'components/Hello'
-import eventBus from '../pages/eventBus/main.vue'
-import vuexTest from '../pages/vuex_test/main.vue'
-import index from '../pages/index.vue'
+// import Hello from 'components/Hello'
+// import eventBus from '../pages/eventBus/main.vue'
+// import vuexTest from '../pages/vuex_test/main.vue'
+// import index from '../pages/index.vue'
 
 export default new Router({
   routes: [
     {
       path: '',
       name: 'index',
-      component: index
+      component: function (resolve) {
+        require.ensure([], function () {
+          resolve(require('../pages/index.vue'))
+        }, 'index')
+      }
     },
     {
       path: '/test',
       name: 'test',
-      component: Hello
+      component: function (resolve) {
+        require.ensure([], function () {
+          resolve(require('../components/Hello.vue'))
+        }, 'test')
+      }
     },
     {
       path: '/event_bus',
       name: 'eventBus',
-      component: eventBus
+      component: function (resolve) {
+        require.ensure([], function () {
+          resolve(require('../pages/eventBus/main.vue'))
+        }, 'eventBus')
+      }
     },
     {
       path: '/vuex_test',
       name: 'vuexTest',
-      component: vuexTest
+      component: function (resolve) {
+        require.ensure([], function () {
+          resolve(require('../pages/vuex_test/main.vue'))
+        }, 'vuexTest')
+      }
     }
   ]
 })
